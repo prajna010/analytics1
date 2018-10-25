309076935f1bff4dee0dece595c5b1d4ee082dff
prajna010 <prajna.nitk@gmail.com>
2018-10-25 14:18:43 +0530

    code1

---
+++ assignment1.R
@@ -0,0 +1,23 @@
+#starting to work in R 
+
+# assign---
+x1 = 3 
+x2<-3
+#print values
+x1
+x2
+#assign and print
+(x3=3)
+#environment---
+#variables in env
+is()
+# datasets available for use
+data()
+womwn
+data(womwn)
+?women
+attach(mtcars)
+mpg
+#libraries currently loaded
+library
+
